5a21f3bf85c5086d4be4cca3bba03ff6b29dd011
AzaharJaveed12 <javeedshareef95199@gmail.com>
2019-06-12 21:53:40 +0530

    :tada: initial commit!

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <app-nav></app-nav>
+      <router-view class='container'/>
+  </div>
+</template>
+<script>
+import appNav from './components/appNav.vue';
+export default {
+  components:{
+    appNav,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to community</h1>
+      <p class="lead">open source projects are available.</p>
+      <hr class="my-4">
+      <p>This app is about community.Be kind Be helpfull..</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">signUp</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+    
+  }
+};
+</script>
